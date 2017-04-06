27bd6bfbc7391c100f801955320bbcd211543589
Gauri Gadkari <gauri.gadkari@gmail.com>
2017-04-05 17:12:43 -0700

    Create Event screen

---
+++ app/src/main/java/com/codepath/insync/activities/CreateEventActivity.java
@@ -0,0 +1,15 @@
+package com.codepath.insync.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.codepath.insync.R;
+
+public class CreateEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_event);
+    }
+}
