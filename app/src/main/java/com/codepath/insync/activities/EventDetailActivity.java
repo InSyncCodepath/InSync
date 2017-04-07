687ba4fcc65be8e642d8a545317aaa348478125e
Unum Sarfraz <anamsarfraz@gmail.com>
2017-04-06 23:25:05 -0700

    Add event detail layout

---
+++ app/src/main/java/com/codepath/insync/activities/EventDetailActivity.java
@@ -0,0 +1,71 @@
+package com.codepath.insync.activities;
+
+import android.databinding.DataBindingUtil;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.support.v7.graphics.Palette;
+
+import com.codepath.insync.R;
+import com.codepath.insync.databinding.ActivityEventDetailBinding;
+import com.codepath.insync.utils.Constants;
+
+
+public class EventDetailActivity extends AppCompatActivity {
+    private static final String TAG = "EventDetailActivity";
+
+    ActivityEventDetailBinding binding;
+    CollapsingToolbarLayout collapsingToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_event_detail);
+        setupToolbar();
+    }
+
+    private void setupToolbar() {
+        collapsingToolbar = binding.ctlEventDetail;
+
+        setSupportActionBar(binding.tbEventDetail);
+        getSupportActionBar().setTitle("");
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),
+                R.drawable.palette);
+        Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
+            @SuppressWarnings("ResourceType")
+            @Override
+            public void onGenerated(Palette palette) {
+                int vibrantColor = palette.getVibrantColor(R.color.primary_light);
+                collapsingToolbar.setContentScrimColor(vibrantColor);
+                collapsingToolbar.setStatusBarScrimColor(R.color.accent);
+            }
+        });
+
+
+        binding.abEventDetail.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+
+                Log.d(TAG, "Appbar offset changed to: "+verticalOffset);
+
+                if (Math.abs(verticalOffset) > 650) {
+                    TextView tvEventName = (TextView) collapsingToolbar.findViewById(R.id.tvEDName);
+                    collapsingToolbar.setTitle(tvEventName.getText().toString());
+                    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+                    binding.rlToolbar.setVisibility(View.INVISIBLE);
+                } else {
+                    collapsingToolbar.setTitle(Constants.EMPTY_STR);
+                    getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+                    binding.rlToolbar.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+    }
+}
--- app/src/main/java/com/codepath/insync/utils/Constants.java
+++ app/src/main/java/com/codepath/insync/utils/Constants.java
@@ -1,8 +1,6 @@
 package com.codepath.insync.utils;
 
-/**
- * Created by usarfraz on 4/3/17.
- */
 
 public class Constants {
+    public static final String EMPTY_STR = "";
 }
