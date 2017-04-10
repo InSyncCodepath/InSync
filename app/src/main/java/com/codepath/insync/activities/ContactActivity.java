08ad4a79354dacbe417041d8edb1a1b623620455
Gauri Gadkari <gauri.gadkari@gmail.com>
2017-04-09 17:05:37 -0700

    Added Contact Permission and contact activity

---
+++ app/src/main/java/com/codepath/insync/activities/ContactActivity.java
@@ -0,0 +1,15 @@
+package com.codepath.insync.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.codepath.insync.R;
+
+public class ContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+    }
+}
---
+++ app/src/main/java/com/codepath/insync/adapters/CursorRecyclerAdapter.java
@@ -0,0 +1,8 @@
+package com.codepath.insync.adapters;
+
+/**
+ * Created by Gauri Gadkari on 4/9/17.
+ */
+
+public class CursorRecyclerAdapter {
+}
