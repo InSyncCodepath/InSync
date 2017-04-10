4ccf59ec3f482e834f96f37d54002f29d7ad6b2c
Gauri Gadkari <gauri.gadkari@gmail.com>
2017-04-09 23:43:07 -0700

    Modified RecyclerView Cursor Adapter to check if a contact is selected

--- app/src/main/java/com/codepath/insync/adapters/SimpleCursorRecyclerAdapter.java
+++ app/src/main/java/com/codepath/insync/adapters/SimpleCursorRecyclerAdapter.java
@@ -7,13 +7,21 @@ package com.codepath.insync.adapters;
 import android.content.Context;
 import android.database.Cursor;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.codepath.insync.R;
+
+import java.util.ArrayList;
+
+import static com.codepath.insync.R.id.isSelected;
 
 public class SimpleCursorRecyclerAdapter extends CursorRecyclerAdapter<SimpleViewHolder> {
 
@@ -22,7 +30,7 @@ public class SimpleCursorRecyclerAdapter extends CursorRecyclerAdapter<SimpleVie
     private int[] mTo;
     private String[] mOriginalFrom;
     private Context context;
-
+    boolean isSelectedContact [];
     public SimpleCursorRecyclerAdapter (Context context, int layout, Cursor c, String[] from, int[] to) {
         super(c);
         this.context = context;
@@ -40,16 +48,33 @@ public class SimpleCursorRecyclerAdapter extends CursorRecyclerAdapter<SimpleVie
     }
 
     @Override
-    public void onBindViewHolder (SimpleViewHolder holder, Cursor cursor) {
+    public void onBindViewHolder (final SimpleViewHolder holder, Cursor cursor, final int position) {
+        if(isSelectedContact == null && cursor.getCount() > 0){
+            isSelectedContact = new boolean[cursor.getCount()];
+        }
         final int count = mTo.length;
         final int[] from = mFrom;
-
+        holder.isSelected.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(isChecked){
+                    isSelectedContact[position] = true;
+                } else {
+                    isSelectedContact[position] = false;
+                }
+            }
+        });
 //        for (int i = 0; i < count; i++) {
 //            holder.views[i].setText(cursor.getString(from[i]));
 //        }
         holder.contactName.setText(cursor.getString(from[0]));
         Glide.with(context).load(cursor.getString(from[1])).into(holder.imageView);
         holder.contactNumber.setText(cursor.getString(from[2]));
+        if(isSelectedContact[position]) {
+            holder.isSelected.setChecked(true);
+        } else {
+            holder.isSelected.setChecked(false);
+        }
     }
 
     /**
@@ -85,6 +110,7 @@ class SimpleViewHolder extends RecyclerView.ViewHolder
 {
     public TextView contactName, contactNumber;
     public ImageView imageView;
+    public CheckBox isSelected;
 
     public SimpleViewHolder (View itemView, int[] to)
     {
@@ -95,5 +121,6 @@ class SimpleViewHolder extends RecyclerView.ViewHolder
         contactName = (TextView) itemView.findViewById(to[0]);
         imageView = (ImageView) itemView.findViewById(to[1]);
         contactNumber = (TextView) itemView.findViewById(to[2]);
+        isSelected = (CheckBox) itemView.findViewById(R.id.isSelected);
     }
 }
