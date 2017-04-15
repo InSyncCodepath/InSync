991c4275fe9a62d53a90acc8a423a1216ebac09f
Unum Sarfraz <anamsarfraz@gmail.com>
2017-04-15 00:09:42 -0700

    Updating chat for the event only

--- app/src/main/java/com/codepath/insync/fragments/MessageSendFragment.java
+++ app/src/main/java/com/codepath/insync/fragments/MessageSendFragment.java
@@ -2,6 +2,7 @@ package com.codepath.insync.fragments;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -11,6 +12,7 @@ import android.widget.Toast;
 
 import com.codepath.insync.R;
 import com.codepath.insync.databinding.FragmentMessageSendBinding;
+import com.codepath.insync.models.parse.Event;
 import com.codepath.insync.models.parse.Message;
 import com.codepath.insync.models.parse.User;
 import com.parse.ParseException;
@@ -20,6 +22,26 @@ import com.parse.SaveCallback;
 public class MessageSendFragment extends Fragment {
     private static String TAG = "MessageSendFragment";
     FragmentMessageSendBinding binding;
+    Event event;
+
+    public static MessageSendFragment newInstance(String eventId) {
+
+        Bundle args = new Bundle();
+
+        MessageSendFragment messageSendFragment = new MessageSendFragment();
+        args.putString("eventId", eventId);
+
+        messageSendFragment.setArguments(args);
+        return messageSendFragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        event = new Event();
+        event.setObjectId(getArguments().getString("eventId"));
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -40,7 +62,7 @@ public class MessageSendFragment extends Fragment {
                 String data = binding.etEDMessage.getText().toString();
 
                 // Using new `Message` Parse-backed model now
-                Message message = new Message();
+                final Message message = new Message();
                 message.setBody(data);
 
                 message.setSender(User.getCurrentUser());
@@ -49,9 +71,20 @@ public class MessageSendFragment extends Fragment {
                     @Override
                     public void done(ParseException e) {
                         if(e == null) {
-                            Toast.makeText(getActivity(), "Successfully created message on Parse",
+                            Toast.makeText(getActivity(), "Your message was successfully sent!",
                                     Toast.LENGTH_SHORT).show();
 
+                            event.getMessageRelation().add(message);
+                            event.updateEvent(new SaveCallback() {
+                                @Override
+                                public void done(ParseException e) {
+                                    if (e == null) {
+                                        Log.d(TAG, "Message was successfully linked to the event.");
+                                    } else {
+                                        Log.e(TAG, "Message link to event failed with error: "+ e.getLocalizedMessage());
+                                    }
+                                }
+                            });
                         } else {
                             Log.e(TAG, "Failed to save message", e);
                         }
