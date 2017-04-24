71e8e880d3ee8ff575301d16ee531543d79afc5c
Unum Sarfraz <anamsarfraz@gmail.com>
2017-04-24 00:25:48 -0700

    Add splash screen

--- app/src/main/java/com/codepath/insync/activities/SplashActivity.java
+++ app/src/main/java/com/codepath/insync/activities/SplashActivity.java
@@ -1,39 +1,59 @@
 package com.codepath.insync.activities;
 
 import android.content.Intent;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+
 
 import com.codepath.insync.R;
 import com.codepath.insync.models.parse.User;
+import com.codepath.insync.utils.Constants;
+import com.daimajia.androidanimations.library.Techniques;
+import com.viksaa.sssplash.lib.activity.AwesomeSplash;
+import com.viksaa.sssplash.lib.cnst.Flags;
+import com.viksaa.sssplash.lib.model.ConfigSplash;
 
-public class SplashActivity extends AppCompatActivity {
-    private final int SPLASH_DISPLAY_LENGTH = 1000;
+public class SplashActivity extends AwesomeSplash {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+    public void initSplash(ConfigSplash configSplash) {
+
+//Customize Circular Reveal
+        configSplash.setBackgroundColor(R.color.lighter_green); //any color you want form colors.xml
+        configSplash.setAnimCircularRevealDuration(2000); //int ms
+        configSplash.setRevealFlagX(Flags.REVEAL_LEFT);  //or Flags.REVEAL_LEFT
+        configSplash.setRevealFlagY(Flags.REVEAL_LEFT); //or Flags.REVEAL_TOP
+
+        //Choose LOGO OR PATH; if you don't provide String value for path it's logo by default
+
+        //Customize Logo
+        configSplash.setLogoSplash(R.drawable.ic_launch); //or any other drawable
+        configSplash.setAnimLogoSplashDuration(2000); //int ms
+        configSplash.setAnimLogoSplashTechnique(Techniques.FadeIn); //choose one form Techniques (ref: https://github.com/daimajia/AndroidViewAnimations)
+
+        //Customize Title
+        configSplash.setTitleSplash(Constants.INSYNC_LOGO);
+        configSplash.setTitleTextColor(R.color.primary_dark);
+        configSplash.setTitleTextSize(36f); //float value
+        configSplash.setAnimTitleDuration(3000);
+        configSplash.setAnimTitleTechnique(Techniques.FlipInX);
+        //configSplash.setTitleFont("fonts/myfont.ttf"); //TODO: ADD FONT
+
+    }
+
+    @Override
+    public void animationsFinished() {
 
         User currentUser = User.getCurrentUser();
         Intent intent;
 
         if (currentUser != null) {
-             intent = new Intent(SplashActivity.this, EventListActivity.class);
+            intent = new Intent(SplashActivity.this, EventListActivity.class);
         } else {
             intent = new Intent(SplashActivity.this, LoginActivity.class);
         }
 
-        final Intent splashIntent = intent;
-        new Handler().postDelayed(new Runnable(){
-            @Override
-            public void run() {
-                startActivity(splashIntent);
-                finish();
-            }
-        }, SPLASH_DISPLAY_LENGTH);
-
+        startActivity(intent);
+        finish();
 
     }
+
 }
