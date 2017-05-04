e3400f1ce59c07c96003eba645b10931d1ddabd5
Unum Sarfraz <anamsarfraz@gmail.com>
2017-05-04 00:31:03 -0700

    Updating notification styling and grouping

--- app/src/main/java/com/codepath/insync/receivers/ParseCBroadcastReceiver.java
+++ app/src/main/java/com/codepath/insync/receivers/ParseCBroadcastReceiver.java
@@ -6,8 +6,14 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.preference.PreferenceManager;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.app.TaskStackBuilder;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 import android.widget.RemoteViews;
@@ -17,19 +23,33 @@ import com.codepath.insync.R;
 import com.codepath.insync.activities.EventDetailActivity;
 import com.codepath.insync.activities.EventListActivity;
 import com.codepath.insync.activities.LoginActivity;
+import com.codepath.insync.models.parse.Event;
+import com.codepath.insync.models.parse.User;
+import com.codepath.insync.models.parse.UserEventRelation;
 import com.codepath.insync.utils.Constants;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.SaveCallback;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.Iterator;
+import java.util.List;
 
+import static android.R.attr.id;
 import static android.R.attr.value;
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
+import static com.facebook.FacebookSdk.getApplicationContext;
 
 
 public  class ParseCBroadcastReceiver extends BroadcastReceiver {
     public static final String intentAction = "com.parse.push.intent.RECEIVE";
+    public static final String yesRsvp = "RSVP_YES";
+    public static final String noRsvp = "RSVP_NO";
     private static final String TAG = "ParseCBroadcastReceiver";
+    private static final String KEY_NOTIFICATION_GROUP = "InSync Notifications";
+    private static final int GROUP_ID = 1;
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -43,6 +63,8 @@ public  class ParseCBroadcastReceiver extends BroadcastReceiver {
 
     private void processPush(Context context, Intent intent) {
         String action = intent.getAction();
+        String eventId = intent.getStringExtra("eventId");
+
         Log.d(TAG, "got action " + action);
         if (action.equals(intentAction)) {
             String channel = intent.getExtras().getString("com.parse.Channel");
@@ -74,7 +96,39 @@ public  class ParseCBroadcastReceiver extends BroadcastReceiver {
             } catch (JSONException ex) {
                 Log.d(TAG, "JSON failed!");
             }
+        } else if (action.equals(yesRsvp)) {
+            Log.d(TAG, "GOT RSVP YES");
+            updateRsvp(eventId, Constants.ATTENDING);
+            // Get the notification manager system service
+            NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            mNotificationManager.cancel(intent.getIntExtra("noti_id", -1));
+
+        } else if (action.equals(noRsvp)) {
+            Log.d(TAG, "GOT RSVP NO");
+            updateRsvp(eventId, Constants.DECLINE);
+            // Get the notification manager system service
+            NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            mNotificationManager.cancel(intent.getIntExtra("noti_id", -1));
         }
+    }
+
+    private void updateRsvp(String eventId, final int rsvpStatus) {
+        Event event = new Event();
+        event.setObjectId(eventId);
+        UserEventRelation.findUserEvent(event, new FindCallback<UserEventRelation>() {
+            @Override
+            public void done(List<UserEventRelation> objects, ParseException e) {
+                Log.d(TAG, "Got "+objects.size()+" UserEventRelation");
+                UserEventRelation userEventRelation = objects.get(0);
+                userEventRelation.setRsvpStatusKey(rsvpStatus);
+                objects.get(0).updateRsvpStatus(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        Log.d(TAG, "RSVP updated successfully");
+                    }
+                });
+            }
+        });
     }
 
     // Create a local dashboard notification to tell user about the event
@@ -98,6 +152,16 @@ public  class ParseCBroadcastReceiver extends BroadcastReceiver {
     // TODO: add activity to launch
     // See: http://guides.codepath.com/android/Using-Intents-to-Create-Flows
     private void createNewEventNoti(Context context, JSONObject notiObj, int notiType) {
+        String eventId = notiObj.optString("eventId");
+        // Create request ID
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        int requestID = sharedPreferences.getInt(eventId, -1);
+        if (requestID == -1) {
+            requestID = (int)System.currentTimeMillis();
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putInt(eventId, requestID);
+            editor.apply();
+        }
 
         // Define custom views
         RemoteViews contentView = new RemoteViews(BuildConfig.APPLICATION_ID, R.layout.notification_invite);
@@ -105,32 +169,81 @@ public  class ParseCBroadcastReceiver extends BroadcastReceiver {
         contentView.setTextViewText(R.id.tvInviteTitle, notiObj.optString("title"));
         contentView.setTextViewText(R.id.tvInviteText, notiObj.optString("text"));
 
+        // Define event list activity to trigger when group notification is selected
+        Intent intent = new Intent(getApplicationContext(), EventListActivity.class);
+        PendingIntent eventListIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
         // Define the intent to trigger when notification is selected
-        Intent resultIntent = new Intent(context.getApplicationContext(), EventDetailActivity.class);
-        resultIntent.putExtra("eventId", notiObj.optString("eventId"));
-        resultIntent.putExtra("isCurrent", true);
+        Intent detailIntent = new Intent(context.getApplicationContext(), EventDetailActivity.class);
+        detailIntent.putExtra("eventId", eventId);
+        detailIntent.putExtra("isCurrent", true);
         TaskStackBuilder stackBuilder = TaskStackBuilder.create(context.getApplicationContext());
         // Adds the back stack
         stackBuilder.addParentStack(EventDetailActivity.class);
         // Adds the Intent to the top of the stack
-        stackBuilder.addNextIntent(resultIntent);
+        stackBuilder.addNextIntent(detailIntent);
         // Gets a PendingIntent containing the entire back stack
-        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent detailPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // Yes intent
+        Intent yesReceive = new Intent();
+        yesReceive.putExtra("eventId", eventId);
+        yesReceive.putExtra("noti_id", requestID);
+        yesReceive.setAction(yesRsvp);
+        PendingIntent pendingIntentYes = PendingIntent.getBroadcast(context, 20, yesReceive, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        //No intent
+        Intent noReceive = new Intent();
+        noReceive.putExtra("eventId", eventId);
+        noReceive.putExtra("noti_id", requestID);
+        noReceive.setAction(noRsvp);
+        PendingIntent pendingIntentNo = PendingIntent.getBroadcast(context, 20, noReceive, PendingIntent.FLAG_UPDATE_CURRENT);
 
         // Now we can attach the pendingIntent to a new notification using setContentIntent
-        Notification notification = new NotificationCompat.Builder(context)
-                .setSmallIcon(R.mipmap.ic_insync_launch)
-                .setContent(contentView)
+        Notification groupNotification = new NotificationCompat.Builder(context)
+                .setSmallIcon(R.mipmap.ic_lock_screen_noti)
+                //.setCustomContentView(contentView)
+                .setStyle(new NotificationCompat.InboxStyle())
+                .setColor(ContextCompat.getColor(context, R.color.tealish))
+                .setContentTitle(notiObj.optString("title"))
+                .setSubText(User.getCurrentUser().getName())
                 .setAutoCancel(true) // Hides the notification after its been selected
                 //.setDefaults(Notification.DEFAULT_ALL)
                 //.setPriority(Notification.PRIORITY_HIGH)
-                .setContentIntent(resultPendingIntent)
-                .setFullScreenIntent(resultPendingIntent, true) // Sets the notification for heads up display
+                .setContentIntent(eventListIntent)
+                //.setFullScreenIntent(detailPendingIntent, false) // Sets the notification for heads up display
+                .setGroupSummary(true)
+                .setGroup(KEY_NOTIFICATION_GROUP)
+                .setWhen(System.currentTimeMillis())
+
                 .build();
-        // Get the notification manager system service
-        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        // mId allows you to update the notification later on.
-        mNotificationManager.notify((int)System.currentTimeMillis(), notification);
+
+        Notification notification = new NotificationCompat.Builder(context)
+                .setSmallIcon(R.mipmap.ic_lock_screen_noti)
+                //.setCustomContentView(contentView)
+                .setStyle(new NotificationCompat.InboxStyle())
+                .setColor(ContextCompat.getColor(context, R.color.tealish))
+                .setContentTitle(notiObj.optString("title"))
+                .setContentText(notiObj.optString("text"))
+                .setSubText(User.getCurrentUser().getName())
+                .setAutoCancel(true) // Hides the notification after its been selected
+                //.setDefaults(Notification.DEFAULT_ALL)
+                //.setPriority(Notification.PRIORITY_HIGH)
+                .setContentIntent(detailPendingIntent)
+                //.setFullScreenIntent(detailPendingIntent, false) // Sets the notification for heads up display
+                .setGroup(KEY_NOTIFICATION_GROUP)
+
+                .addAction(R.mipmap.ic_lock_screen_noti, "Attending", pendingIntentYes)
+                .addAction(R.drawable.ic_decline_unselected, "Regret", pendingIntentNo)
+                .addAction(R.drawable.icon, "Details", detailPendingIntent)
+                .setWhen(System.currentTimeMillis())
+
+                .build();
+            // Get the notification manager system service
+            //NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            NotificationManagerCompat manager = NotificationManagerCompat.from(context);
+            manager.notify(GROUP_ID, groupNotification);
+            manager.notify(requestID, notification);
     }
     private void launchSomeActivity(Context context, String datavalue) {
         Intent pupInt = new Intent(context, LoginActivity.class);
